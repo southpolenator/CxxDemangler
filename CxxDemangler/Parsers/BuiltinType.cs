e29d4eeb38694b1ae41c94bbfb821ddbccd9b4be
Vuk Jovanovic <southpolenator@gmail.com>
2017-08-21 00:35:30 +0200

    Adding BuiltinType parsing tests.

--- CxxDemangler/Parsers/BuiltinType.cs
+++ CxxDemangler/Parsers/BuiltinType.cs
@@ -1,5 +1,37 @@
 ﻿namespace CxxDemangler.Parsers
 {
+    // <builtin-type> ::= v  # void
+    //                ::= w  # wchar_t
+    //                ::= b  # bool
+    //                ::= c  # char
+    //                ::= a  # signed char
+    //                ::= h  # unsigned char
+    //                ::= s  # short
+    //                ::= t  # unsigned short
+    //                ::= i  # int
+    //                ::= j  # unsigned int
+    //                ::= l  # long
+    //                ::= m  # unsigned long
+    //                ::= x  # long long, __int64
+    //                ::= y  # unsigned long long, __int64
+    //                ::= n  # __int128
+    //                ::= o  # unsigned __int128
+    //                ::= f  # float
+    //                ::= d  # double
+    //                ::= e  # long double, __float80
+    //                ::= g  # __float128
+    //                ::= z  # ellipsis
+    //                ::= Dd # IEEE 754r decimal floating point (64 bits)
+    //                ::= De # IEEE 754r decimal floating point (128 bits)
+    //                ::= Df # IEEE 754r decimal floating point (32 bits)
+    //                ::= Dh # IEEE 754r half-precision floating point (16 bits)
+    //                ::= DF <number> _ # ISO/IEC TS 18661 binary floating point type _FloatN (N bits)
+    //                ::= Di # char32_t
+    //                ::= Ds # char16_t
+    //                ::= Da # auto
+    //                ::= Dc # decltype(auto)
+    //                ::= Dn # std::nullptr_t (i.e., decltype(nullptr))
+    //                ::= u <source-name>    # vendor extended type
     internal class BuiltinType : IParsingResult
     {
         public static IParsingResult Parse(ParsingContext context)
@@ -26,12 +58,12 @@
 
         internal class Extension : BuiltinType
         {
-            private IParsingResult name;
-
             public Extension(IParsingResult name)
             {
-                this.name = name;
+                Name = name;
             }
+
+            public IParsingResult Name { get; private set; }
         }
     }
 }
---
+++ Tests/Parsing/BuiltinType.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CxxDemangler.Tests.Parsing
+{
+    [TestClass]
+    public class BuiltinType : TestBase
+    {
+        [TestMethod]
+        public void BuiltinType1()
+        {
+            Verify("c...",
+                new Parsers.StandardBuiltinType(Parsers.StandardBuiltinType.Values.Char));
+        }
+
+        [TestMethod]
+        public void BuiltinType2()
+        {
+            Verify("c",
+                new Parsers.StandardBuiltinType(Parsers.StandardBuiltinType.Values.Char), "");
+        }
+
+        [TestMethod]
+        public void BuiltinTypeExtension()
+        {
+            Verify("u3abc...",
+                new Parsers.BuiltinType.Extension(
+                    new Parsers.SourceName.Identifier("abc")));
+        }
+
+        [TestMethod]
+        public void BuiltinTypeFailures()
+        {
+            Assert.IsNull(Parse("."));
+            Assert.IsNull(Parse(""));
+        }
+
+        internal override IParsingResult Parse(ParsingContext context)
+        {
+            return Parsers.BuiltinType.Parse(context);
+        }
+    }
+}
