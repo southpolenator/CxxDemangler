f226011ea1461c80324060ad26f2a80c2b83d0ee
Vuk Jovanovic <southpolenator@gmail.com>
2017-08-21 01:26:35 +0200

    Adding SimpleOperatorName parsing tests.

---
+++ Tests/Parsing/SimpleOperatorName.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CxxDemangler.Tests.Parsing
+{
+    [TestClass]
+    public class SimpleOperatorName : TestBase
+    {
+        [TestMethod]
+        public void SimpleOperatorNameNoTrailing()
+        {
+            Verify("qu",
+                new Parsers.SimpleOperatorName(Parsers.SimpleOperatorName.Values.Question),
+                endsWith: "");
+        }
+
+        [TestMethod]
+        public void SimpleOperatorNameTrailing()
+        {
+            Verify("quokka",
+                new Parsers.SimpleOperatorName(Parsers.SimpleOperatorName.Values.Question),
+                endsWith: "okka");
+        }
+
+        [TestMethod]
+        public void SimpleOperatorNameFailures()
+        {
+            Assert.IsNull(Parse("bu-buuuu"));
+            Assert.IsNull(Parse("q"));
+            Assert.IsNull(Parse(""));
+        }
+
+        internal override IParsingResult Parse(ParsingContext context)
+        {
+            return Parsers.SimpleOperatorName.Parse(context);
+        }
+    }
+}
