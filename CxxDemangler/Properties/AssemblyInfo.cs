f0c7eb2213d47e20b7aca13044c9953f9ea17b20
Vuk Jovanovic <southpolenator@users.noreply.github.com>
2017-08-24 08:50:39 +0200

    Singing assembly so that other signed assemblies can reference it.

--- CxxDemangler/Properties/AssemblyInfo.cs
+++ CxxDemangler/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4b0b87f5-0c25-47dc-b507-709faa1020a2")]
-[assembly: InternalsVisibleTo("CxxDemangler.Tests")]
+[assembly: InternalsVisibleTo("CxxDemangler.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001003511cd1d7d6df45dbccb7001fd12c3c2e5171f59e80ebae225e7e4e0d80aece5d2fec51849b76a033e37049d2880cb5dbbe3f3e10e8dc0aeda8c54a2caa321e4f527e400a235bd6a8158926dce4a1e1cf11ebce2e1a804c086ed1068414561eb2dff5433fab52c45c33f790950996ea303cdd643325605d384cc3fc8ee066baf")]
 
 
 // Version information for an assembly consists of the following four values:
